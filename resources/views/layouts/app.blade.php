20bd8bfcd4cec0e590e0a7df4544d044f7a40471
Bulanadviani <advianibulanfitria@gmail.com>
2025-08-01 23:13:01 +0700

    logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -258,7 +258,6 @@
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    
 
     <!-- Backend Bundle JavaScript -->
     <script src="{{ asset('assets/js/backend-bundle.min.js') }}"></script>
